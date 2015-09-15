f16724782097a3181260ba20d9132f163b12845c
Jan von Cosel <janvc@gmx.de>
2015-09-15 09:55:57 +0200

    some numerical changes in mctdh_spec_fft

--- mctdh_spec_fft/mctdh_spec_fft.m
+++ mctdh_spec_fft/mctdh_spec_fft.m
@@ -38,9 +38,9 @@ end
 dt = (input_data(2,1) - input_data(1,1)) * fs2au;
 nyquist = 2.0 * pi / (2.0 * dt);
 time = (0.0 : dt : (N - 1) * dt)';
-double_time = (-(N-1)*dt : dt : (N - 1) * dt)';
+double_time = (-(N-1)*dt : dt : N * dt)';
 df = 2.0 * nyquist / N;
-double_df = 2.0 * nyquist / (2*N-2);
+double_df = 2.0 * nyquist / (2*N);
 freq = (-nyquist : df : nyquist)';
 double_freq = (-nyquist : double_df : nyquist)';
 
@@ -50,11 +50,11 @@ corr(1:length(input_data)) = input_data(:,2) + j * input_data(:,3);
 corr_damp = corr .* exp(-time / tau) .* cos(pi * time / (2.0 * time(N)));
 
 % construct the mirrored autocorrelation function with cosine weighting and exponential damping
-double_corr = zeros(2*N-1,1);
+double_corr = zeros(2*N,1);
 for a=0:N-1
   double_corr(a+1) = conj(corr(N-a));   % complex conjugate the negative part!!!
   double_corr(2*N-1-a) = corr(N-a);
-endfor
+end
 double_corr_damp = double_corr .* cos(pi * double_time / (2.0 * time(N))) .* exp(-abs(double_time) / tau);
 
 % this is the actual fourier transformation.
@@ -63,18 +63,11 @@ double_spec = ifft(double_corr_damp);
 
 % properly arrange the positive and negative parts of the spectrum.
 spec_shift = zeros(N+1,1);
-for i=0:(length(spec) / 2)
-  spec_shift(i+1) = spec((length(spec) / 2)+i);
-endfor
-for i=1:(length(spec) / 2)
-  spec_shift((length(spec) / 2)+1+i) = spec(i);
-endfor
-double_spec_shift = zeros(2*N-1,1);
-double_spec_shift(N) = double_spec(1);
-for a=1:N-1
-  double_spec_shift(N-a) = double_spec(length(double_spec)-a);
-  double_spec_shift(N+a) = double_spec(1+a);
-endfor
+double_spec_shift = zeros(2*N+1,1);
+spec_shift(1:N/2) = spec((N/2)+1:N);
+spec_shift((N/2)+1:N+1) = spec(1:(N/2)+1);
+double_spec_shift(1:N) = double_spec(N+1:2*N);
+double_spec_shift(N+1:2*N+1) = double_spec(1:N+1);
 
 % write the results to the output files.
 specfile = fopen("spec_out.dat", "w");
@@ -85,15 +78,15 @@ fprintf(autofile, "#         Time [au]        Re(Auto)         Im(Auto)\n");
 fprintf(specfile, "# Frequency [au]    Frequency [cm-1]        Re(Spec)          Im(Spec)\n");
 fprintf(double_autofile, "#         Time [au]        Re(Auto)         Im(Auto)\n");
 fprintf(double_specfile, "# Frequency [au]    Frequency [cm-1]        Re(Spec)          Im(Spec)\n");
-for i=1:N
-  fprintf(autofile, "%20.10f %16.10f %16.10f\n", time(i), real(corr_damp(i)), imag(corr_damp(i)));
-  fprintf(specfile, "%15.10f %17.10f %17.10f\n", freq(i), real(spec_shift(i)), imag(spec_shift(i)));
-endfor
+for a=1:N
+  fprintf(autofile, "%20.10f %16.10f %16.10f\n", time(a), real(corr_damp(a)), imag(corr_damp(a)));
+  fprintf(specfile, "%15.10f %17.10f %17.10f\n", freq(a), real(spec_shift(a)), imag(spec_shift(a)));
+end
 fprintf(specfile, "%15.10f %17.10f %17.10f\n", freq(N+1), real(spec_shift(N+1)), imag(spec_shift(N+1)));
 for a=1:2*N-1
   fprintf(double_autofile, "%20.10f %16.10f %16.10f\n", double_time(a), real(double_corr_damp(a)), imag(double_corr_damp(a)));
   fprintf(double_specfile, "%15.10f %17.10f %17.10f\n", double_freq(a), real(double_spec_shift(a)), imag(double_spec_shift(a)));
-endfor
+end
 fclose(specfile);
 fclose(autofile);
 fclose(double_specfile);
