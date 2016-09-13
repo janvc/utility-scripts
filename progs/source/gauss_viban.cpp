ff79f1f5014cf45431422ca92e89a34cceca1864
Jan von Cosel <janvc@gmx.de>
2016-09-13 16:14:40 +0200

    changed the output format in gauss_viban

--- progs/source/gauss_viban.cpp
+++ progs/source/gauss_viban.cpp
@@ -31,7 +31,6 @@
 #include "constants.h"
 #include "vibrationalanalysis.h"
 
-void gram_schmidt(Eigen::MatrixXd &d);
 
 int main(int argc, char *argv[])
 {
@@ -68,102 +67,90 @@ int main(int argc, char *argv[])
 	/*
 	 * write the results to stdout
 	 */
-	std::cout << "Number of atoms: " << vibAn.Natoms() << std::endl;
+	std::cout << "Performing vibrational analysis of file '" << filename << "'" << std::endl << std::endl;
+	std::cout << "Number of atoms:        " << std::setw(6) << vibAn.Natoms() << std::endl;
+	std::cout << "Number of normal modes: " << std::setw(6) << vibAn.Nmodes() << std::endl << std::endl;
 
 	std::cout << "atomic coordinates in bohr:\n";
 	WriteVector(vibAn.X(), digits, clean, threshold);
 
+	std::cout << std::endl;
+
 	vibAn.prtMinGeo();
 
-	std::cout << "atomic masses in u:\n";
+	std::cout << "\natomic masses in u:\n";
 	WriteVector(vibAn.masses(), digits, clean, threshold);
 
-	std::cout << "total mass in au: " << vibAn.totalMass() << std::endl;
+	std::cout << "\ntotal mass in au: " << vibAn.totalMass() << std::endl << std::endl;
 
 	std::cout << "non-mass-weighted cartesian hessian\n";
-	WriteMatrix(vibAn.Fcart(), digits, clean, threshold);
+	WriteSymmGaussMatrix(vibAn.Fcart());
 
-	std::cout << "mass-weighted cartesian hessian\n";
-	WriteMatrix(vibAn.Fmwc(), digits, clean, threshold);
+	std::cout << "\nmass-weighted cartesian hessian\n";
+	WriteSymmGaussMatrix(vibAn.Fmwc());
 
-	std::cout << "eigenvalues of the mass-weighted cartesian hessian (au and cm-1):\n";
+	std::cout << "\neigenvalues of the mass-weighted cartesian hessian (au and cm-1):\n";
 	for (int i = 0; i < vibAn.Ncoords(); i++)
-		std::cout << std::scientific << std::setprecision(4) << std::setw(12) << double(vibAn.mwcFC()(i))
-				  << "  " << std::fixed << std::setw(9) << double(vibAn.mwcFrq()(i)) << std::endl;
+		std::cout << std::scientific << std::setprecision(4)
+				  << std::setw(5) << i + 1
+				  << std::setw(14) << double(vibAn.mwcFC()(i))
+				  << "  " << std::fixed << std::setw(10) << double(vibAn.mwcFrq()(i)) << std::endl;
 
-	std::cout << "center of mass in bohr:\n";
+	std::cout << "\ncenter of mass in bohr:\n";
 	WriteVector(vibAn.com(), digits, clean, threshold);
 
-	std::cout << "inertia tensor:\n";
-	WriteMatrix(vibAn.inert(), digits, clean, threshold);
+	std::cout << "\ninertia tensor:\n";
+	WriteSymmGaussMatrix(vibAn.inert());
 
-	std::cout << "moments of inertia:\n";
+	std::cout << "\nmoments of inertia:\n";
 	WriteVector(vibAn.moments(), digits, clean, threshold);
 
-	std::cout << "principal axes:\n";
-	WriteMatrix(vibAn.prinAxes(), digits, clean, threshold);
+	std::cout << "\nprincipal axes:\n";
+	WriteGaussMatrix(vibAn.prinAxes());
 
-	std::cout << "metric of the principal axes:\n";
-	WriteMatrix(vibAn.prinAxes().transpose() * vibAn.prinAxes(), digits, clean, threshold);
+	std::cout << "\nmetric of the principal axes:\n";
+	WriteSymmGaussMatrix(vibAn.prinAxes().transpose() * vibAn.prinAxes());
 
-	std::cout << "The D matrix:\n";
-	WriteMatrix(vibAn.D(), digits, clean, threshold);
+	std::cout << "\nThe D matrix:\n";
+	WriteGaussMatrix(vibAn.D());
 
-	std::cout << "metric of D matrix:\n";
-	WriteMatrix(vibAn.D().transpose() * vibAn.D(), digits, clean, threshold);
+	std::cout << "\nmetric of D matrix:\n";
+	WriteSymmGaussMatrix(vibAn.D().transpose() * vibAn.D());
 
-	std::cout << "The Nvib x Nvib submatrix of the internal Hessian, according to eq. (6):\n";
-	WriteMatrix(vibAn.Fint(), digits, clean, threshold);
+	std::cout << "\nThe Nvib x Nvib submatrix of the internal Hessian, according to eq. (6):\n";
+	WriteSymmGaussMatrix(vibAn.Fint());
 
-	std::cout << "eigenvalues of the internal hessian (au and cm-1):\n";
+	std::cout << "\neigenvalues of the internal hessian (au and cm-1):\n";
 	for (int i = 0; i < vibAn.Nmodes(); i++)
-		std::cout << std::scientific << std::setprecision(4) << std::setw(12) << double(vibAn.intFC()(i))
-				  << "  " << std::fixed << std::setw(9) << double(vibAn.intFrq()(i)) << std::endl;
+		std::cout << std::scientific << std::setprecision(4)
+				  << std::setw(5) << i + 1
+				  << std::setw(14) << double(vibAn.intFC()(i))
+				  << "  " << std::fixed << std::setw(10) << double(vibAn.intFrq()(i)) << std::endl;
 
-	std::cout << "eigenvectors of the internal Hessian (internal displacements):\n";
-	WriteMatrix(vibAn.Lint(), digits, clean, threshold);
+	std::cout << "\neigenvectors of the internal Hessian (internal displacements):\n";
+	WriteGaussMatrix(vibAn.Lint());
 
-	std::cout << "metric of the eigenvectors:\n";
-	WriteMatrix(vibAn.Lint().transpose() * vibAn.Lint(), digits, clean, threshold);
+	std::cout << "\nmetric of the eigenvectors:\n";
+	WriteSymmGaussMatrix(vibAn.Lint().transpose() * vibAn.Lint());
 
-	std::cout << "mass-weighted cartesian displacements:\n";
-	WriteMatrix(vibAn.Lmwc(), digits, clean, threshold);
+	std::cout << "\nmass-weighted cartesian displacements:\n";
+	WriteGaussMatrix(vibAn.Lmwc());
 
-	std::cout << "metric of mass-weighted cartesian displacements:\n";
-	WriteMatrix(vibAn.Lmwc().transpose() * vibAn.Lmwc(), digits, clean, threshold);
+	std::cout << "\nmetric of mass-weighted cartesian displacements:\n";
+	WriteSymmGaussMatrix(vibAn.Lmwc().transpose() * vibAn.Lmwc());
 
-	std::cout << "cartesian displacements:\n";
-	WriteMatrix(vibAn.Lcart(), digits, clean, threshold);
+	std::cout << "\ncartesian displacements:\n";
+	WriteGaussMatrix(vibAn.Lcart());
+
+	std::cout << std::endl;
 
 	vibAn.prtMinModes();
 
-	std::cout << "metric of cartesian displacements:\n";
-	WriteMatrix(vibAn.Lcart().transpose() * vibAn.Lcart(), digits, clean, threshold);
+	std::cout << "\nmetric of the cartesian displacements:\n";
+	WriteSymmGaussMatrix(vibAn.Lcart().transpose() * vibAn.Lcart());
 
-	std::cout << "reduced masses of the normal modes:\n";
+	std::cout << "\nreduced masses of the normal modes:\n";
 	WriteVector(vibAn.mu(), digits, clean, threshold);
-
-	Eigen::VectorXd xshift = vibAn.X() + vibAn.Lcart().col(56) * 0.03 / ang2a0;
-
-	std::cout << "\nThe shifted structure:\n";
-	std::cout << "------------------------------------------\n";
-	for (int i = 0; i < vibAn.Natoms(); i++)
-		std::cout << std::fixed << std::setprecision(6)
-	<< std::setw(6) << i + 1
-	<< std::setw(12) << double(xshift(3*i+0)) * ang2a0
-	<< std::setw(12) << double(xshift(3*i+1)) * ang2a0
-	<< std::setw(12) << double(xshift(3*i+2)) * ang2a0 << std::endl;
-	std::cout << "------------------------------------------\n";
-
-	std::cout << "\nDifference between the original and the shifted structure:\n";
-	std::cout << "------------------------------------------\n";
-	for (int i = 0; i < vibAn.Natoms(); i++)
-		std::cout << std::fixed << std::setprecision(6)
-	<< std::setw(6) << i + 1
-	<< std::setw(12) << (double(xshift(3*i+0)) - double(vibAn.X()(3*i+0))) * ang2a0
-	<< std::setw(12) << (double(xshift(3*i+1)) - double(vibAn.X()(3*i+1))) * ang2a0
-	<< std::setw(12) << (double(xshift(3*i+2)) - double(vibAn.X()(3*i+2))) * ang2a0 << std::endl;
-	std::cout << "------------------------------------------\n";
 
 	return 0;
 }
